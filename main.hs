88a21893f4616509329ac2e935e9c4216779ef9c
Mjturn <Mjturn@outlook.com>
2024-06-14 15:30:19 -0400

    validate user input

--- main.hs
+++ main.hs
@@ -1,22 +1,24 @@
 import Text.Printf
+import Text.Read (readMaybe)
 
 calculateEuclideanDistance :: Double -> Double -> Double -> Double -> Double
 calculateEuclideanDistance x1 y1 x2 y2 = sqrt((x1 - x2)^2 + (y1 - y2)^2)
 
+validateInput :: String -> IO Double
+validateInput prompt = do
+    putStrLn prompt
+    input <- getLine
+    case readMaybe input of
+        Just number -> return number
+        Nothing -> do
+            putStrLn "Sorry, the input you provided is invalid. Please try again."
+            validateInput prompt
+
 main = do
-    putStrLn "Enter x1's value."
-    x1 <- getLine
-    putStrLn "Enter y1's value."
-    y1 <- getLine
-    putStrLn "Enter x2's value."
-    x2 <- getLine
-    putStrLn "Enter y2's value."
-    y2 <- getLine
+    x1 <- validateInput "Enter x1's value."
+    y1 <- validateInput "Enter y1's value."
+    x2 <- validateInput "Enter x2's value."
+    y2 <- validateInput "Enter y2's value."
 
-    let x1' = read x1 :: Double
-    let y1' = read y1 :: Double
-    let x2' = read x2 :: Double
-    let y2' = read y2 :: Double
-
-    let euclideanDistance = calculateEuclideanDistance x1' y1' x2' y2'
-    printf "The Euclidean distance between points (%f, %f) and (%f, %f) is %f.\n" x1' y1' x2' y2' euclideanDistance
+    let euclideanDistance = calculateEuclideanDistance x1 y1 x2 y2
+    printf "The Euclidean distance between points (%f, %f) and (%f, %f) is %f.\n" x1 y1 x2 y2 euclideanDistance
